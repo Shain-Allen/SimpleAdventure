8c3a4b7bf159aae1647ee7993e27a8c860c767f0
Shain-Allen <shainallen96@yahoo.com>
2021-05-27 09:43:07 -0700

    Progress on player class

--- Player.cs
+++ Player.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 namespace SimpleAdventure
 {
 	public class Player
@@ -9,11 +11,58 @@ namespace SimpleAdventure
 		private int atk;
 		private Weapon weapon = null;
 		private Armor armor = null;
-		private Item[] inventory;
+		private List<Item> inventory;
 
-		public Player(string _Name, int _Health, int _Def, int _Atk, Weapon _weapon, Armor _Armor, Item[] inventory)
+
+		public Player(string _Name, int _Health, int _Def, int _Atk, Weapon _weapon, Armor _Armor, Item[] _Inventory)
 		{
-
+			this.name = _Name;
+			this.health = _Health;
+			this.def = _Def;
+			this.atk = _Atk;
+			this.weapon = _weapon;
+			this.armor = _Armor;
+			foreach (Item item in _Inventory)
+			{
+				this.inventory.Add(item);
+			}
 		}
+
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+		public bool EquipBetterItem(Item item, string type)
+		{
+			type.ToLower();
+
+			if (type == "weapon")
+			{
+				return true;
+			}
+			else if (type == "armor")
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		//returns true if item was added and false if item could not be added
+		public bool itemToInv(Item item)
+		{
+			if (item == null)
+			{
+				return false;
+			}
+
+			inventory.Add(item);
+			return true;
+		}
+
+
 	}
 }
